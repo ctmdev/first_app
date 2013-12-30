9cfdf654bbf7c0911f92d705c89b62b77d9652dc
U-ctmillarTerm\ctmillar <ctmillar@ctmillarTerm.(none)>
2013-12-30 01:09:08 -0600

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '6f910ef92512dfb0d6d11a1768ddd493199e4dcac4fdafa3fb60e754124b448ab620323ba4de8eecbca9ed0c1ce06769b9afc9e75f74d6dcf3660342aa682826'
